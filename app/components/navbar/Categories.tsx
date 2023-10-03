7353e2b0b8235c7198dc65a3afd3ed422205f656
Manami <vaattii1996@gmail.com>
2023-10-02 17:11:17 -0700

    modify categories

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -23,7 +23,17 @@ export const categories = [
   {
     label: "Beach",
     icon: TbBeach,
-    description: "This property is close to beach!",
+    description: "This property is close to the beach!",
+  },
+  {
+    label: "Windmills",
+    icon: GiWindmill,
+    description: "This property is has windmills!",
+  },
+  {
+    label: "Modern",
+    icon: MdOutlineVilla,
+    description: "This property is modern!",
   },
   {
     label: "Countryside",
@@ -33,37 +43,57 @@ export const categories = [
   {
     label: "Pools",
     icon: TbPool,
-    description: "This property has a pool!",
+    description: "This is property has a beautiful pool!",
   },
   {
-    label: "Island",
+    label: "Islands",
     icon: GiIsland,
     description: "This property is on an island!",
   },
   {
+    label: "Lake",
+    icon: GiBoatFishing,
+    description: "This property is near a lake!",
+  },
+  {
     label: "Skiing",
     icon: FaSkiing,
-    description: "This property has skiing activities!",
+    description: "This property has skiing activies!",
+  },
+  {
+    label: "Castles",
+    icon: GiCastle,
+    description: "This property is an ancient castle!",
+  },
+  {
+    label: "Caves",
+    icon: GiCaveEntrance,
+    description: "This property is in a spooky cave!",
+  },
+  {
+    label: "Camping",
+    icon: GiForestCamp,
+    description: "This property offers camping activities!",
   },
   {
     label: "Arctic",
     icon: BsSnow,
-    description: "This property is in a snow area!",
+    description: "This property is in arctic environment!",
   },
   {
-    label: "Cave",
-    icon: GiCaveEntrance,
-    description: "This property is in a cave!",
+    label: "Desert",
+    icon: GiCactus,
+    description: "This property is in the desert!",
   },
   {
     label: "Barns",
     icon: GiBarn,
-    description: "This property is in the barn!",
+    description: "This property is in a barn!",
   },
   {
     label: "Lux",
     icon: IoDiamond,
-    description: "This property is luxurious!",
+    description: "This property is brand new and luxurious!",
   },
 ];
 
